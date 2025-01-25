66902114f6adadd15be1a5a7fdb123aaabb54f6b
jiwoo <kimjiwoo410@gmail.com>
2025-01-25 10:40:09 +0900

    Commit Measage

--- src/main/java/me/silvernine/tutorial/controller/CarController.java
+++ src/main/java/me/silvernine/tutorial/controller/CarController.java
@@ -20,13 +20,13 @@ public class CarController {
 
     private final CarService carService;
 
-    @Operation(summary = "차량 등록", description = "차량 데이터를 관리하는 API로, 차량을 등록하거나 조회하는 기능을 제공합니다")
+    @Operation(summary = "차량 등록", description = "차량 데이터를 관리하는 API로, 차량을 등록하는 기능을 제공합니다")
     @PostMapping
     public ResponseEntity<CarResponse> registerCar(@RequestBody CarRequest request, Principal principal) {
         return ResponseEntity.ok(carService.registerCar(request, principal));
     }
 
-    @Operation(summary = "Get user's cars", description = "Fetches a list of cars registered by the authenticated user")
+    @Operation(summary = "차량 전체 조회", description = "차량 데이터를 관리하는 API로, 차량을 조회하는 기능을 제공합니다")
     @GetMapping("/user")
     public ResponseEntity<List<CarResponse>> getUserCars(Principal principal) {
         return ResponseEntity.ok(carService.getUserCars(principal));
