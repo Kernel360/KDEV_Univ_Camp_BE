ad23a7a24f6fe5e7a03a3a893045ff4119c511fe
jiwoo <kimjiwoo410@gmail.com>
2025-02-03 20:08:29 +0900

    Commit Measage

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,23 @@
-insert into authority (authority_name) values ('ROLE_USER');
-insert into authority (authority_name) values ('ROLE_ADMIN');
+-- 1️⃣ 권한 테이블에 데이터 삽입 (가장 먼저 실행)
+INSERT INTO authority (authority_name) VALUES ('ROLE_USER');
+INSERT INTO authority (authority_name) VALUES ('ROLE_ADMIN');
 
-insert into "user" (username, password, nickname, activated) values ('admin', '$2a$08$lDnHPz7eUkSi6ao14Twuau08mzhWrL4kyZGGU5xfiGALO/Vxd5DOi', 'admin', 1);
-insert into "user" (username, password, nickname, activated) values ('user', '$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC', 'user', 1);
+-- 2️⃣ 사용자 테이블에 데이터 삽입 (`user`는 예약어이므로 백틱 사용)
+INSERT INTO `user` (username, password, nickname, activated) 
+VALUES ('admin', '$2a$08$lDnHPz7eUkSi6ao14Twuau08mzhWrL4kyZGGU5xfiGALO/Vxd5DOi', 'admin', 1);
 
-insert into user_authority (user_id, authority_name) values (1, 'ROLE_USER');
-insert into user_authority (user_id, authority_name) values (1, 'ROLE_ADMIN');
-insert into user_authority (user_id, authority_name) values (2, 'ROLE_USER');
+INSERT INTO `user` (username, password, nickname, activated) 
+VALUES ('user', '$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC', 'user', 1);
+
+-- 3️⃣ 삽입된 사용자 ID 확인 (MySQL에서는 auto_increment가 보장되지 않음)
+SELECT user_id, username FROM `user`;
+
+-- 4️⃣ user_authority 테이블에 데이터 삽입 (`user_id` 값 확인 후 조정 필요)
+INSERT INTO user_authority (user_id, authority_name) 
+VALUES ((SELECT user_id FROM `user` WHERE username='admin'), 'ROLE_USER');
+
+INSERT INTO user_authority (user_id, authority_name) 
+VALUES ((SELECT user_id FROM `user` WHERE username='admin'), 'ROLE_ADMIN');
+
+INSERT INTO user_authority (user_id, authority_name) 
+VALUES ((SELECT user_id FROM `user` WHERE username='user'), 'ROLE_USER');
