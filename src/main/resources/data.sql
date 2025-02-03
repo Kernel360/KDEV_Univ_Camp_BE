2cc5441e7cb0f363b6669921a02483b6252ce1ee
jiwoo <kimjiwoo410@gmail.com>
2025-02-03 19:26:26 +0900

    Commit Measage

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,14 @@
-insert into "user" (username, password, nickname, activated) values ('admin', '$2a$08$lDnHPz7eUkSi6ao14Twuau08mzhWrL4kyZGGU5xfiGALO/Vxd5DOi', 'admin', 1);
-insert into "user" (username, password, nickname, activated) values ('user', '$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC', 'user', 1);
+-- 기본 사용자 삽입
+INSERT IGNORE INTO user (username, password, nickname, activated) 
+VALUES 
+('admin', '$2a$08$lDnHPz7eUkSi6ao14Twuau08mzhWrL4kyZGGU5xfiGALO/Vxd5DOi', 'admin', 1),
+('user', '$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC', 'user', 1);
 
-insert into authority (authority_name) values ('ROLE_USER');
-insert into authority (authority_name) values ('ROLE_ADMIN');
+-- 권한 추가 (중복 삽입 방지)
+INSERT IGNORE INTO authority (authority_name) VALUES ('ROLE_USER');
+INSERT IGNORE INTO authority (authority_name) VALUES ('ROLE_ADMIN');
 
-insert into user_authority (user_id, authority_name) values (1, 'ROLE_USER');
-insert into user_authority (user_id, authority_name) values (1, 'ROLE_ADMIN');
-insert into user_authority (user_id, authority_name) values (2, 'ROLE_USER');
+-- 사용자 권한 매핑
+INSERT IGNORE INTO user_authority (user_id, authority_name) VALUES (1, 'ROLE_USER');
+INSERT IGNORE INTO user_authority (user_id, authority_name) VALUES (1, 'ROLE_ADMIN');
+INSERT IGNORE INTO user_authority (user_id, authority_name) VALUES (2, 'ROLE_USER');
