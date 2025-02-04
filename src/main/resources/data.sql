687f34e4f4a4c2a4397640cc7237bc633f51edea
jiwoo <kimjiwoo410@gmail.com>
2025-02-04 11:17:41 +0900

    Commit Measage

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,9 +2,9 @@
 INSERT IGNORE INTO authority (authority_name) VALUES ('ROLE_ADMIN');
 
 -- admin 계정이 없는 경우에만 추가
-INSERT IGNORE INTO `user` (username, password, nickname, activated)
+INSERT IGNORE INTO `user` (id, password, nickname, activated)
 VALUES ('admin', '$2a$08$lDnHPz7eUkSi6ao14Twuau08mzhWrL4kyZGGU5xfiGALO/Vxd5DOi', 'admin', 1);
 
 -- user_authority 테이블에 ROLE_ADMIN이 없을 경우만 추가
 INSERT IGNORE INTO user_authority (user_id, authority_name)
-VALUES ((SELECT user_id FROM `user` WHERE username='admin'), 'ROLE_ADMIN');
+VALUES ((SELECT user_id FROM `user` WHERE id='admin'), 'ROLE_ADMIN');
