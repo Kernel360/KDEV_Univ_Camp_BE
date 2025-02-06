d7e28f19c1ad20f344fc972973976f3d0aceb00c
jiwoo <kimjiwoo410@gmail.com>
2025-02-06 11:44:27 +0900

    Commit Measage

--- src/main/java/me/silvernine/tutorial/repository/UserRepository.java
+++ src/main/java/me/silvernine/tutorial/repository/UserRepository.java
@@ -5,8 +5,10 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, String> {
+public interface UserRepository extends JpaRepository<User, Long> { // 🔥 변경: userId가 PK이므로 Long으로 설정
 
     @EntityGraph(attributePaths = "authorities") // ✅ 권한 정보 함께 가져오기
     Optional<User> findOneWithAuthoritiesById(String id);
+
+    boolean existsById(String id); // 🔥 추가: id 중복 체크
 }
