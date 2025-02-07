037923fb27ae11c69d9db8fcfb4c0f26a4f8b965
jiwoo <kimjiwoo410@gmail.com>
2025-02-07 13:25:45 +0900

    Commit Message

--- src/main/java/me/silvernine/tutorial/dto/UserDto.java
+++ src/main/java/me/silvernine/tutorial/dto/UserDto.java
@@ -1,8 +1,9 @@
 package me.silvernine.tutorial.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 import me.silvernine.tutorial.entity.User;
 
@@ -14,7 +15,7 @@ import jakarta.validation.constraints.Size;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@JsonPropertyOrder({"id", "nickname"}) // ✅ JSON 응답 순서 지정 (비밀번호 제거)
+@JsonPropertyOrder({"id", "password", "nickname"}) // ✅ JSON 응답 순서 지정
 public class UserDto {
 
    @NotNull
@@ -22,9 +23,10 @@ public class UserDto {
    @JsonProperty("id")
    private String id;
 
-   @JsonIgnore // ✅ JSON 응답에서 비밀번호 제외
    @NotNull
    @Size(min = 3, max = 100)
+   @JsonInclude(JsonInclude.Include.NON_NULL)  // ✅ null 값이 아닐 때만 JSON 포함
+   @Schema(description = "사용자의 비밀번호", example = "mysecurepassword")  // ✅ Swagger 문서화
    private String password;
 
    @NotNull
