0069c0e8714bd32eda75bd97c1c405f53e914312
jiwoo <kimjiwoo410@gmail.com>
2025-02-19 15:11:50 +0900

    배치 테스트용 1분 간격 실행.

--- src/main/java/me/silvernine/tutorial/batch/DailyStatisticsBatch.java
+++ src/main/java/me/silvernine/tutorial/batch/DailyStatisticsBatch.java
@@ -9,7 +9,8 @@ import java.time.LocalDate;
 @Component // Spring Bean 등록
 public class DailyStatisticsBatch {
 
-    @Scheduled(cron = "0 5 0 * * ?") // 매일 00:05 실행
+    //@Scheduled(cron = "0 5 0 * * ?") // 매일 00:05 실행
+    @Scheduled(cron = "0 * * * * ?") //테스트용 1분 간격 실행
     public void runDailyBatch() {
         LocalDate yesterday = LocalDate.now().minusDays(1);
         log.info("📊 [일 단위 배치] {} 데이터 통계 계산 시작...", yesterday);
