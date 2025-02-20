573113722eb39570634df74ce50e3fc8c6f19e4c
jiwoo <kimjiwoo410@gmail.com>
2025-02-20 18:12:12 +0900

    UNIX_TIMESTAMP()를 사용.

--- src/main/java/me/silvernine/tutorial/repository/TripRepository.java
+++ src/main/java/me/silvernine/tutorial/repository/TripRepository.java
@@ -25,11 +25,12 @@ public interface TripRepository extends JpaRepository<Trip, Long> {
     // ✅ 차량 번호 + 특정 기간의 GPS 데이터 중 interval 간격으로 필터링된 데이터 조회
     @Query("SELECT t FROM Trip t WHERE t.carNumber = :carNumber " +
             "AND t.timestamp BETWEEN :startDateTime AND :endDateTime " +
-            "AND FUNCTION('MOD', FUNCTION('TO_SECONDS', t.timestamp), :interval) = 0 " +
+            "AND MOD(FUNCTION('UNIX_TIMESTAMP', t.timestamp), :interval) = 0 " +
             "ORDER BY t.timestamp")
     List<Trip> findByCarNumberAndInterval(
             @Param("carNumber") String carNumber,
             @Param("startDateTime") LocalDateTime startDateTime,
             @Param("endDateTime") LocalDateTime endDateTime,
             @Param("interval") int interval);
+
 }
