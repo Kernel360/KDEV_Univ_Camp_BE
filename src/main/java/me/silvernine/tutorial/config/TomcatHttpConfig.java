eb0ba39111e9ce2d3d32d60f635d6cbea2f2ef82
jiwoo <kimjiwoo410@gmail.com>
2025-02-25 14:33:59 +0900

    8080 허용.

---
+++ src/main/java/me/silvernine/tutorial/config/TomcatHttpConfig.java
@@ -0,0 +1,26 @@
+package me.silvernine.tutorial.config;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TomcatHttpConfig {
+
+    @Bean
+    public TomcatServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(httpConnector());
+        return tomcat;
+    }
+
+    private Connector httpConnector() {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(8080);  // HTTP 포트 추가
+        connector.setSecure(false);
+        connector.setRedirectPort(8443); // HTTP → HTTPS 리다이렉트 (선택사항)
+        return connector;
+    }
+}
