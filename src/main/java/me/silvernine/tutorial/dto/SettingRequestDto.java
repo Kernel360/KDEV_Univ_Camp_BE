25f3d0021d8dfd50b1396f193b66fbc5bf1df7b6
kimjiwoo0707 <144203709+kimjiwoo0707@users.noreply.github.com>
2025-12-09 20:14:54 +0900

    Update SettingRequestDto.java

--- src/main/java/me/silvernine/tutorial/dto/SettingRequestDto.java
+++ src/main/java/me/silvernine/tutorial/dto/SettingRequestDto.java
@@ -6,8 +6,8 @@ import lombok.Setter;
 @Getter
 @Setter
 public class SettingRequestDto {
-    private String mdn;    //차량 번호
-    private String oTime;  //발생 일시
-    private String ctrCnt; //제어명령 개수
-    private String geoCnt; //지오펜싱 설정 개수
+    private String mdn;
+    private String oTime;
+    private String ctrCnt;
+    private String geoCnt;
 }
