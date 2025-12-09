d2aa8aebe229cb3843f76aa7bcd165265d89485e
kimjiwoo0707 <144203709+kimjiwoo0707@users.noreply.github.com>
2025-12-09 20:12:34 +0900

    Update ControlInfoRequestDto.java

--- src/main/java/me/silvernine/tutorial/dto/ControlInfoRequestDto.java
+++ src/main/java/me/silvernine/tutorial/dto/ControlInfoRequestDto.java
@@ -8,27 +8,27 @@ import java.util.List;
 @Getter
 @Setter
 public class ControlInfoRequestDto {
-    private String mdn;       // 차량 번호
-    private String tid;       // 터미널 아이디
-    private String mid;       // 제조사 아이디
-    private String pv;        // 패킷 버전
-    private String did;       // 디바이스 아이디
-    private String onTime;    // 차량 시동 On 시간
-    private String dFWVer;    // 디바이스 펌웨어 버전
-    private String oTime;     // 발생 시간
-    private String cCnt;      // 주기 데이터 개수
-    private List<CycleData> cList; // 주기 데이터 리스트
+    private String mdn;
+    private String tid;
+    private String mid;
+    private String pv;
+    private String did;
+    private String onTime;
+    private String dFWVer;
+    private String oTime;
+    private String cCnt;
+    private List<CycleData> cList;
 
     @Getter
     @Setter
     public static class CycleData {
-        private String sec; // 초 단위
-        private String gcd; // GPS 상태
-        private String lat; // 위도
-        private String lon; // 경도
-        private String ang; // 방향
-        private String spd; // 속도
-        private String sum; // 누적 주행 거리
-        private String bat; // 배터리 전압
+        private String sec;
+        private String gcd;
+        private String lat;
+        private String lon;
+        private String ang;
+        private String spd;
+        private String sum;
+        private String bat;
     }
 }
