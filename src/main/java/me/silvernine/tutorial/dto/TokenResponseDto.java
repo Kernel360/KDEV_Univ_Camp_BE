52c7a0b9ed2a7fa56e00aa5b7fd045bff82777db
kimjiwoo0707 <144203709+kimjiwoo0707@users.noreply.github.com>
2025-12-09 20:16:27 +0900

    Update TokenResponseDto.java

--- src/main/java/me/silvernine/tutorial/dto/TokenResponseDto.java
+++ src/main/java/me/silvernine/tutorial/dto/TokenResponseDto.java
@@ -6,9 +6,9 @@ import lombok.Getter;
 @Getter
 @Builder
 public class TokenResponseDto {
-    private String rstCd; // 응답 코드
-    private String rstMsg; // 응답 메시지
-    private String mdn;    // 차량 번호
-    private String token;  // 단말인증 토큰
-    private String exPeriod; // 만료 기간
+    private String rstCd;
+    private String rstMsg;
+    private String mdn;
+    private String token;
+    private String exPeriod;
 }
