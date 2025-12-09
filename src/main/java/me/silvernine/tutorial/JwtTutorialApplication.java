15211f93fed3db869ee50ddcc0e8e5c5cb354abf
kimjiwoo0707 <144203709+kimjiwoo0707@users.noreply.github.com>
2025-12-09 20:32:40 +0900

    Update JwtTutorialApplication.java

--- src/main/java/me/silvernine/tutorial/JwtTutorialApplication.java
+++ src/main/java/me/silvernine/tutorial/JwtTutorialApplication.java
@@ -1,4 +1,4 @@
-package me.silvernine.tutorial; // ✅ 올바른 패키지로 변경
+package me.silvernine.tutorial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
