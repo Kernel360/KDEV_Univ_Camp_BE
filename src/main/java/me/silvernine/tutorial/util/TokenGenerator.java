eb91d013c320ee56abb5d74f2043e02e973bdaf0
kimjiwoo0707 <144203709+kimjiwoo0707@users.noreply.github.com>
2025-12-09 20:31:42 +0900

    Update TokenGenerator.java

--- src/main/java/me/silvernine/tutorial/util/TokenGenerator.java
+++ src/main/java/me/silvernine/tutorial/util/TokenGenerator.java
@@ -3,7 +3,6 @@ package me.silvernine.tutorial.util;
 import java.util.UUID;
 
 public class TokenGenerator {
-
     public static String createToken(String mdn, String tid) {
         return UUID.randomUUID().toString().replace("-", "") + ":" + mdn + ":" + tid;
     }
